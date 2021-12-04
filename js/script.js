bceea34cd5f70d32ed614f892fd337fb34e56acc
Jessica T <88570634+jmcclung3509@users.noreply.github.com>
2021-12-03 21:52:15 -0800

    Created function to retrieve repo data

--- js/script.js
+++ js/script.js
@@ -2,6 +2,8 @@
 const overview = document.querySelector(".overview");
 //Github profile name//
 const username = "jmcclung3509";
+// List where repos will be displayed//
+const reposList = document.querySelector(".repo-list");
 
 const getInfo = async function () {
     const res = await fetch (`https://api.github.com/users/${username}`);
@@ -20,10 +22,30 @@ const displayUserInfo = function (data) {
     </figure>
     <div>
         <p><strong>Name:</strong> ${data.name}</p>
-        <p></strong>Bio:</strong> ${data.bio}</p>
+        <p><strong>Bio:</strong> ${data.bio}</p>
         <p><strong>Location:</strong> ${data.location}</p>
         <p><strong> Number of public repos</strong> ${data.public_repos}</p>
     </div>
     `
     overview.append(userDiv);
+    getRepos();
+};
+
+const getRepos = async function () {
+    const fetchRepo = await fetch (`https://api.github.com/users/${username}/repos?sort=updated&per_page=100`);
+    const repoData = await fetchRepo.json();
+    console.log(repoData);
+    displayRepoInfo(repoData);
+};
+
+const displayRepoInfo = function (repos) {
+    for (const repo of repos) {
+        const li = document.createElement("li");
+        li.classList.add ("repo");
+        li.innerHTML = `
+        <h3> ${repo.name}</h3>`
+        reposList.append(li);
+    }
+
+
 };
